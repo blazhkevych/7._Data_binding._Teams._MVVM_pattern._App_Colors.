0fecae5071553bc9ef936bdf43b6db5abdbf88b9
Виктор Блажкевич <blazhkevych.va@gmail.com>
2023-02-07 16:46:15 +0200

    ы

---
+++ task/Model/ColorModel.cs
@@ -0,0 +1,33 @@
+﻿namespace task.Model;
+
+public class ColorModel
+{
+    public ColorModel(byte alpha, byte red, byte green, byte blue)
+    {
+        Alpha = alpha;
+        Red = red;
+        Green = green;
+        Blue = blue;
+    }
+
+    public byte Alpha { get; set; }
+
+    public byte Red { get; set; }
+
+    public byte Green { get; set; }
+
+    public byte Blue { get; set; }
+
+    public override string ToString()
+    {
+        return $"{ToHex()}";
+        //return $"ARGB: ({Alpha}, {Red}, {Green}, {Blue})";
+    }
+
+    // Метод конвертирует цвет из формата ARGB в формат "FF48BD00" и возвращает строку.
+    private string ToHex()
+    {
+        var hex = Alpha.ToString("X2") + Red.ToString("X2") + Green.ToString("X2") + Blue.ToString("X2");
+        return hex;
+    }
+}
---
+++ task/ViewModel/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace task.ViewModel;
+
+// Базовый класс для всех ViewModel.
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    // Событие, которое вызывается при изменении свойства.
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // Метод, который вызывает событие PropertyChanged.
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
